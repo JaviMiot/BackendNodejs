514bb82d0c22ef0b761ccd221d82060ddb34dfa8
Javier Manobanda <51manobandajavier@gmail.com>
2021-12-03 11:57:36 -0500

    fix virtual field in migration

--- db/migrations/20211202164526-order.js
+++ db/migrations/20211202164526-order.js
@@ -1,6 +1,36 @@
 'use strict';
+const { DataTypes, Sequelize } = require('sequelize');
+const { CUSTOMER_TABLE } = require('../models/customer.model');
 
-const { ORDER_TABLE, OrderSchema } = require('.././models/order.model');
+
+const { ORDER_TABLE } = require('.././models/order.model');
+
+const OrderSchema = {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER
+  },
+  customerId: {
+    field: 'customer_id',
+    allowNull: false,
+    type: DataTypes.INTEGER,
+    references: {
+      model: CUSTOMER_TABLE,
+      key: 'id'
+    },
+    onUpdate: 'CASCADE',
+    onDelete: 'SET NULL'
+  },
+  createdAt: {
+    allowNull: false,
+    type: DataTypes.DATE,
+    field: 'created_at',
+    defaultValue: Sequelize.NOW,
+  },
+};
+
 
 module.exports = {
   up: async (queryInterface) => {
