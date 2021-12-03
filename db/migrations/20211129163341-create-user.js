04051b68b73c38fa85ad9bb3d20c427fa4339047
Javier Manobanda <51manobandajavier@gmail.com>
2021-12-03 11:43:04 -0500

    fix migrations

--- db/migrations/20211129163341-create-user.js
+++ db/migrations/20211129163341-create-user.js
@@ -1,13 +1,42 @@
 'use strict';
+const { Sequelize, DataTypes } = require('sequelize');
 
-const {UserSchema, USER_TABLE} = require('../models/user.model')
+const { USER_TABLE } = require('../models/user.model');
+
+const UserSchema = {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER
+  },
+
+  email: {
+    allowNull: false,
+    type: DataTypes.STRING,
+    unique: true,
+  },
+
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+
+  //* field define el nombre que tendra en la bd
+  createAt: {
+    allowNull: false,
+    type: DataTypes.DATE,
+    field: 'create_at',
+    defaultValue: Sequelize.NOW
+  }
+};
 
 module.exports = {
   up: async (queryInterface) => {
-    await queryInterface.createTable(USER_TABLE, UserSchema)
+    await queryInterface.createTable(USER_TABLE, UserSchema);
   },
 
   down: async (queryInterface) => {
-    await queryInterface.dropTable(USER_TABLE)
+    await queryInterface.dropTable(USER_TABLE);
   }
 };
--- db/migrations/20211129172404-add-role.js
+++ db/migrations/20211129172404-add-role.js
@@ -1,9 +1,17 @@
 'use strict';
-const { UserSchema, USER_TABLE } = require('../models/user.model');
+const { DataTypes } = require('sequelize');
+
+const { USER_TABLE } = require('../models/user.model');
+
+const role = {
+  type: DataTypes.STRING,
+  allowNull: false,
+  defaultValue: 'customer'
+};
 
 module.exports = {
   up: async (queryInterface) => {
-    await queryInterface.addColumn(USER_TABLE, 'role', UserSchema.role);
+    await queryInterface.addColumn(USER_TABLE, 'role', role);
   },
 
   down: async (queryInterface) => {
