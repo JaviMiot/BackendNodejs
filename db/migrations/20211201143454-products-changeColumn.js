2e957367a6096072cea8383985aa80b0bad64e28
Javier Manobanda <51manobandajavier@gmail.com>
2021-12-06 14:37:46 -0500

    fix bug deploy production migration change name price column

--- db/migrations/20211201143454-products-changeColumn.js
+++ db/migrations/20211201143454-products-changeColumn.js
@@ -8,11 +8,6 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.renameColumn(PRODUCT_TABLE, 'price', 'Price');
   }
 };
